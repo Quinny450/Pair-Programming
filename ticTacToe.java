689f0b6a1c95a2b79df5f6c0beb53492356b0b70
Quinn Moore <127361464+Quinny450@users.noreply.github.com>
2025-02-28 12:42:11 -0800

    Refactor ticTacToe class for improved readability and fix minor typos

--- ticTacToe.java
+++ ticTacToe.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class ticTacToe
-{
-    public static void main(String[] args)
-    {
+public class ticTacToe {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int isWon = 0;
         String turn1 = "X";
@@ -12,13 +10,12 @@ public class ticTacToe
         ticTacToeMethods game = new ticTacToeMethods();
 
         System.out.println("Welcome to Tic Tac Toe!");
-        System.out.println("The number assiciated with each cell is as follows:");
+        System.out.println("The number associated with each cell is as follows:");
         System.out.println("[1] [2] [3]");
         System.out.println("[4] [5] [6]");
         System.out.println("[7] [8] [9]");
 
-        while (isWon < 9)
-        {
+        while (isWon < 9) {
             // Turn 1 input
             System.out.println("Player 1, please enter the cell you would like to place your X in:");
             int cellSelection = input.nextInt();
@@ -31,35 +28,32 @@ public class ticTacToe
 
             ticTacToeMethods.printBoard(turn1, row1, col1);
 
-            if (ticTacToeMethods.checkWin())
-            {
+            if (ticTacToeMethods.checkWin()) {
                 System.out.println("Player 1 wins!");
                 break;
             }
 
             isWon++;
 
-            //Check for turn limit
-            if (isWon >= 9)
-            {
+            // Check for turn limit
+            if (isWon >= 9) {
                 System.out.println("The game is a draw!");
                 break;
             }
 
-            //Turn 2 input
+            // Turn 2 input
             System.out.println("Player 2, please enter the cell you would like to place your O in:");
             int cellSelection2 = input.nextInt();
             input.nextLine();
-            
+
             System.out.println("Prompt for String input into cellSelection2");
-            
+
             int row2 = ticTacToeMethods.getRow(cellSelection2);
             int col2 = ticTacToeMethods.getColumn(cellSelection2);
-            
+
             ticTacToeMethods.printBoard(turn2, row2, col2);
 
-            if (ticTacToeMethods.checkWin())
-            {
+            if (ticTacToeMethods.checkWin()) {
                 System.out.println("Player 2 wins!");
                 break;
             }
@@ -67,6 +61,6 @@ public class ticTacToe
             isWon++;
         }
 
-    input.close();
+        input.close();
     }
 }
