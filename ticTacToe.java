942f5ca8cef5cb9b56316569f51ebaf31c531994
Quinn Moore <127361464+Quinny450@users.noreply.github.com>
2025-03-04 09:21:30 -0800

    Add suppression for unused warning in ticTacToe main method

--- ticTacToe.java
+++ ticTacToe.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class ticTacToe {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
