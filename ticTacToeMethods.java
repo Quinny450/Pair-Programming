9417a56ab15c37c2103d5c954faf866707d0ad5f
Quinn Moore <127361464+Quinny450@users.noreply.github.com>
2025-03-06 09:01:54 -0800

    Refactor ticTacToeMethods for consistent brace placement and improved readability

--- ticTacToeMethods.java
+++ ticTacToeMethods.java
@@ -1,7 +1,8 @@
 public class ticTacToeMethods {
     public static String[][] gameState;
 
-    public ticTacToeMethods() {
+    public ticTacToeMethods()
+    {
         gameState = new String[][] {
             {"[ ]", "[ ]", "[ ]"},
             {"[ ]", "[ ]", "[ ]"},
@@ -9,66 +10,81 @@ public class ticTacToeMethods {
         };
     }
 
-    public static int getColumn(int cellSelection) {
-        if (cellSelection == 1 || cellSelection == 4 || cellSelection == 7) {
+    public static int getColumn(int cellSelection)
+    {
+        if (cellSelection == 1 || cellSelection == 4 || cellSelection == 7)
+        {
             return 0;
-        } else if (cellSelection == 2 || cellSelection == 5 || cellSelection == 8) {
+        }
+        else if (cellSelection == 2 || cellSelection == 5 || cellSelection == 8)
+        {
             return 1;
-        } else {
+        }
+        else
+        {
             return 2;
         }
     }
 
-    public static int getRow(int cellSelection) {
-        if (cellSelection == 1 || cellSelection == 2 || cellSelection == 3) {
+    public static int getRow(int cellSelection)
+    {
+        if (cellSelection == 1 || cellSelection == 2 || cellSelection == 3)
+        {
             return 0;
-        } else if (cellSelection == 4 || cellSelection == 5 || cellSelection == 6) {
+        }
+        else if (cellSelection == 4 || cellSelection == 5 || cellSelection == 6)
+        {
             return 1;
-        } else {
+        }
+        else
+        {
             return 2;
         }
     }
 
-    public static boolean checkWin() {
+    public static boolean checkWin()
+    {
         // Check rows
-        for (String[] row : gameState) {
-            if (row[0].equals(row[1]) && row[1].equals(row[2]) && !row[0].equals("[ ]")) {
+        for (String[] row : gameState)
+        {
+            if (row[0].equals(row[1]) && row[1].equals(row[2]) && !row[0].equals("[ ]"))
+            {
                 return true;
             }
         }
         // Check columns
-        for (int col = 0; col < gameState[0].length; col++) {
-            if (gameState[0][col].equals(gameState[1][col]) && gameState[1][col].equals(gameState[2][col]) && !gameState[0][col].equals("[ ]")) {
+        for (int col = 0; col < gameState[0].length; col++)
+        {
+            if (gameState[0][col].equals(gameState[1][col]) && gameState[1][col].equals(gameState[2][col]) && !gameState[0][col].equals("[ ]"))
+            {
                 return true;
             }
         }
-        // Check diagonal 1
-        if (gameState[0][0].equals(gameState[1][1]) && gameState[1][1].equals(gameState[2][2]) && !gameState[0][0].equals("[ ]")) {
+        // Check diagonals
+        if (gameState[0][0].equals(gameState[1][1]) && gameState[1][1].equals(gameState[2][2]) && !gameState[0][0].equals("[ ]"))
+        {
             return true;
         }
-        // Check diagonal 2
-        if (gameState[0][2].equals(gameState[1][1]) && gameState[1][1].equals(gameState[2][0]) && !gameState[0][2].equals("[ ]")) {
+        if (gameState[0][2].equals(gameState[1][1]) && gameState[1][1].equals(gameState[2][0]) && !gameState[0][2].equals("[ ]"))
+        {
             return true;
         }
-
         return false;
     }
 
-    // Checks if tile is already taken
-    public static boolean checkTile(int row, int col) {
-        if (gameState[row][col].equals("[ ]")) {
-            return true;
-        } else {
-            return false;
-        }
+    public static boolean checkTile(int row, int col)
+    {
+        return gameState[row][col].equals("[ ]");
     }
 
-    // Prints the gameState array
-    public static void printBoard(String val, int row, int col) {
-        gameState[row][col] = "[" + val + "]";
-        for (int r = 0; r < gameState.length; r++) {
-            for (int c = 0; c < gameState[0].length; c++) {
-                System.out.print(gameState[r][c] + " ");
+    public static void printBoard(String symbol, int row, int col)
+    {
+        gameState[row][col] = "[" + symbol + "]";
+        for (String[] rowArray : gameState)
+        {
+            for (String cell : rowArray)
+            {
+                System.out.print(cell + " ");
             }
             System.out.println();
         }
